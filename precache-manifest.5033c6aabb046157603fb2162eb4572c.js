71d444d2a6317c004fda132c37cfc53266bf59cb
Sodiq <sodiq.akanmu001@gmail.com>
2020-09-15 15:01:13 +0100

    Updates

---
+++ precache-manifest.5033c6aabb046157603fb2162eb4572c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "921eb81bc13da98678d250fb70cc657f",
+    "url": "/login007/index.html"
+  },
+  {
+    "revision": "061b675ac1e83fbabf5f",
+    "url": "/login007/static/css/main.ff3e4119.chunk.css"
+  },
+  {
+    "revision": "45a201a9b7a040cb3647",
+    "url": "/login007/static/js/2.1a49c073.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/login007/static/js/2.1a49c073.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "061b675ac1e83fbabf5f",
+    "url": "/login007/static/js/main.fb145937.chunk.js"
+  },
+  {
+    "revision": "c8d422749e7abae15e33",
+    "url": "/login007/static/js/runtime-main.fb55f380.js"
+  },
+  {
+    "revision": "a8950e9728f896f21dd1a64e94e7564f",
+    "url": "/login007/static/media/chicken-and-fries.a8950e97.jpg"
+  }
+]);
